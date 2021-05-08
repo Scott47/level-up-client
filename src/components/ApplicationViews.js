20dd0462cd2d0473c2c8b6a74181e1de21ff54bb
Scott47 <scottsilver47@gmail.com>
2021-05-08 17:47:31 -0500

    fixed navbar to reflect current routs and hooked up form for registering a new game

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,10 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./games/GameList"
+import { EventList } from "./events/EventList"
 import { GameProvider } from "./games/GameProvider"
+import { EventProvider } from "./events/EventProvider"
+import { GameForm } from "./games/GameForm"
 
 export const ApplicationViews = () => {
     return <>
@@ -9,11 +12,20 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             backgroundColor: "lightgoldenrodyellow"
         }}>
+            <Route exact path='/'>Welcome to Level Up</Route>
             <GameProvider>
-                <Route exact path="/">
+                <Route exact path="/games">
                     <GameList />
                 </Route>
+                <Route exact path="/games/new">
+                    <GameForm />
+                </Route>
             </GameProvider>
+            <EventProvider>
+                <Route exact path="/events">
+                    <EventList />
+                </Route>
+            </EventProvider>
         </main>
     </>
 }
--- src/components/games/GameProvider.js
+++ src/components/games/GameProvider.js
@@ -4,19 +4,48 @@ export const GameContext = React.createContext()
 
 export const GameProvider = (props) => {
     const [ games, setGames ] = useState([])
+    const [ gameTypes, setTypes ] = useState([])
+
+    const levelUpToken = localStorage.getItem("lu_token")
 
     const getGames = () => {
         return fetch("http://localhost:8000/games", {
             headers:{
-                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+                "Authorization": `Token ${levelUpToken}`
             }
         })
             .then(response => response.json())
             .then(setGames)
     }
 
+    const createGame = (game) => {
+        return fetch("http://localhost:8000/games", { 
+            method: "POST",
+            headers: {
+                'Authorization': `Token ${levelUpToken}`,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(game)
+        })
+            .then(getGames)
+    }
+    
+    const getGameTypes = () => {
+        return fetch("http://localhost:8000/games", { 
+            headers: {
+                "Authorization": `Token ${levelUpToken}`
+            }
+        })
+            .then(setTypes)
+    }
+
     return (
-        <GameContext.Provider value={{ games, getGames }} >
+        <GameContext.Provider value={{ 
+            games,
+            getGames,
+            gameTypes,
+            getGameTypes 
+            }} >
             { props.children }
         </GameContext.Provider>
     )
